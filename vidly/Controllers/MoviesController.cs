bb462b4a22e9b66c9be0f7082655b78911b24576
ahmedjafry <ahmedjafree@gmail.com>
2021-06-15 02:50:04 +0500

    https://github.com/ahmedjafry/test.git

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+using vidly.ViewModels;
+
+namespace vidly.Controllers
+{
+    //Controller class
+    public class MoviesController : Controller
+    {
+        // GET: Movies/
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+
+        }
+
+        private object GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+    }
+}
