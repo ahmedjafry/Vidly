eb7c3a22575a1eb44e85929428975d655bdfc1b7
ahmedjafry <ahmedjafree@gmail.com>
2021-06-16 02:57:05 +0500

    exception error occur

--- vidly/Controllers/CustomerController.cs
+++ vidly/Controllers/CustomerController.cs
@@ -5,22 +5,33 @@ using System.Web;
 using System.Web.Mvc;
 using vidly.Models;
 using vidly.ViewModels;
+using System.Data.Entity;
+
 
 namespace vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+            public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -28,13 +39,6 @@ namespace vidly.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-        {
-            new Customer { Id = 1, Name = "John Smith" },
-            new Customer { Id = 2, Name = "Mary Williams" }
-        };
-        }
+
     }
 }
