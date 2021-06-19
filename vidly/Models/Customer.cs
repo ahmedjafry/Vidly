21830edc9acf122cb9590ab53bc7848c150f966e
ahmedjafry <ahmedjafree@gmail.com>
2021-06-19 22:31:04 +0500

    Section 4 done

--- vidly/Models/Customer.cs
+++ vidly/Models/Customer.cs
@@ -9,12 +9,18 @@ namespace vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MemberShipTypeId { get; set; }
+
+        [Display(Name = "Date Of Birth")]
         public DateTime? Birthdate { get; set; }
+        
     }
 }
